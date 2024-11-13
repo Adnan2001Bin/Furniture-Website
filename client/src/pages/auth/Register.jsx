939be1f1a39caa57236068a3a4c7a764475d83ce
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-13 19:08:18 +0600

    Form for register

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import CommonForm from "@/components/common/Form";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
+const initialState = {
+  useName: "",
+  email: "",
+  password: "",
+};
 function Register() {
+  const [formData, setFormData] = useState(initialState);
+
+  console.log(formData);
+
+  function onSubmit() {}
+
   return (
-    <div>
-      register
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Create new account
+        </h1>
+        <p className="mt-2">
+          Already have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/login"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={registerFormControls}
+        buttonText={"Sign Up"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
