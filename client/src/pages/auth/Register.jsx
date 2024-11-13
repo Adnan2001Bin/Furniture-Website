d2bb1ebef75c3c795c486dd87a719aeac30265c4
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-13 19:12:23 +0600

    fixe error

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,4 +1,5 @@
 import CommonForm from "@/components/common/Form";
+import { registerFormControls } from "@/Config";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
