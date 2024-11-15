19bac8c27abe7932b31d17335b63ff8347c712a1
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-15 17:41:09 +0600

    extraReducers add

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -18,7 +18,7 @@ export const registerUser = createAsyncThunk(
         withCredentials: true,
       }
     );
-    return response.data
+    return response.data;
   }
 );
 
@@ -27,6 +27,23 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     setUser: (state, action) => {},
+  },
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.isAuthenticated = false;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      });
   },
 });
 
