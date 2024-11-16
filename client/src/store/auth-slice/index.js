6227d19540b3ed5809bfacee98f3470837dc25b1
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-16 22:49:53 +0600

    add createAsyncThunk and  extraReducers for login

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -22,13 +22,31 @@ export const registerUser = createAsyncThunk(
   }
 );
 
-const authSlice = createSlice({
+export const loginUser = createAsyncThunk(
+  "/auth/login",
+  async (formData, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/auth/login",
+        formData,
+        {
+          withCredentials: true,
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error during login:", error);
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
+
+const authslice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     setUser: (state, action) => {},
   },
-
   extraReducers: (builder) => {
     builder
       .addCase(registerUser.pending, (state) => {
@@ -43,9 +61,22 @@ const authSlice = createSlice({
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload;
+        state.isAuthenticated = true;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
       });
   },
 });
 
-export const { setUser } = authSlice.actions;
-export default authSlice.reducer;
+export const { setUser } = authslice.actions;
+export default authslice.reducer;
