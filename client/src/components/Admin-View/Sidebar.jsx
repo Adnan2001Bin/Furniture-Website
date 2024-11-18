412a9e7d9a216bb31ea1f648ef86da77d581eb45
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-18 22:13:01 +0600

    basic sidbar

--- client/src/components/Admin-View/Sidebar.jsx
+++ client/src/components/Admin-View/Sidebar.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import {
+  BadgeCheck,
+  ChartNoAxesCombined,
+  LayoutDashboard,
+  ShoppingBasket,
+} from "lucide-react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function AdminSidebar() {
+const adminSidebarMenuItems = [
+  {
+    id: "dashboard",
+    label: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <LayoutDashboard />,
+  },
+  {
+    id: "products",
+    label: "Products",
+    path: "/admin/products",
+    icon: <ShoppingBasket />,
+  },
+  {
+    id: "orders",
+    label: "Orders",
+    path: "/admin/orders",
+    icon: <BadgeCheck />,
+  },
+];
+
+
+function MenuItems() {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>AdminSidebar</h1>
-    </div>
-  )
+    <nav className="mt-8 flex-col flex gap-2">
+      {adminSidebarMenuItems.map((menuItem) => (
+        <div
+          key={menuItem.id}
+          className="flex cursor-pointer text-xl items-center gap-2 rounded-md px-3 py-2 text-muted-foreground hover:bg-muted hover:text-foreground"
+        >
+          {menuItem.icon}
+          <span>{menuItem.label}</span>
+        </div>
+      ))}
+    </nav>
+  );
 }
 
-export default AdminSidebar
+function AdminSidebar() {
+  const navigate = useNavigate();
+  return (
+    <aside className="hidden w-64 flex-col border-r bg-background p-6 lg:flex">
+      <div
+        onClick={() => navigate("/admin/dashboard")}
+        className="flex cursor-pointer items-center gap-2"
+      >
+        <ChartNoAxesCombined size={30} />
+
+        <h1 className="text-2xl font-extrabold">Admin Panel</h1>
+      </div>
+
+      <MenuItems />
+    </aside>
+  );
+}
+
+export default AdminSidebar;
