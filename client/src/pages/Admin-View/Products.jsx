2599c778782ad2c26437c966207204d3387fb946
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-18 23:39:37 +0600

    basic AdminProducts add

--- client/src/pages/Admin-View/Products.jsx
+++ client/src/pages/Admin-View/Products.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import CommonForm from "@/components/common/Form";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { addProductFormElements } from "@/Config";
 
+import React, { Fragment, useState } from "react";
+
+const initialFormData = {
+  image: null,
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  totalStock: "",
+  averageReview: 0,
+};
 function AdminProducts() {
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+    useState(false);
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  function onSubmit() {}
+  console.log(formData, "productList");
+
   return (
-    <div>
-      <h1>AdminProducts</h1>
-    </div>
-  )
+    <Fragment>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={() => setOpenCreateProductsDialog(true)}>
+          Add New Product
+        </Button>
+      </div>
+
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
+        <Sheet
+          open={openCreateProductsDialog}
+          onOpenChange={() => {
+            setOpenCreateProductsDialog(false);
+          }}
+        >
+          <SheetContent side="right" className="overflow-auto">
+            <SheetHeader>
+              <SheetTitle>Add New Product</SheetTitle>
+            </SheetHeader>
+
+            <div className="py-6">
+              <CommonForm
+                onSubmit={onSubmit}
+                formData={formData}
+                setFormData={setFormData}
+                formControls={addProductFormElements}
+                className="py-6"
+              />
+            </div>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </Fragment>
+  );
 }
 
-export default AdminProducts
+export default AdminProducts;
