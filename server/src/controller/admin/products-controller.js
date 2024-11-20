a15dba00a381ee539b3712c6adb5fb06876d37a2
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-20 18:38:30 +0600

    delete a product

--- server/src/controller/admin/products-controller.js
+++ server/src/controller/admin/products-controller.js
@@ -130,4 +130,30 @@ const editProduct = async (req, res) => {
 };
 
 
-export { handleImageUpload, addProduct ,fetchAllProducts};
+//delete a product
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product)
+      return res.status(404).json({
+        success: false,
+        message: "Product not found",
+      });
+
+    res.status(200).json({
+      success: true,
+      message: "Product delete successfully",
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
+
+export { handleImageUpload, addProduct ,fetchAllProducts ,editProduct ,deleteProduct};
