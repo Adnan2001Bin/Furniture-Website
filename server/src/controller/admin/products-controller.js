70b15ebd284455b2d8a3253ca3d720b23a31755b
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-20 18:04:35 +0600

    add a new product

--- server/src/controller/admin/products-controller.js
+++ server/src/controller/admin/products-controller.js
@@ -1,3 +1,5 @@
+import { Product } from "../../models/Product";
+
 const handleImageUpload = async (req, res) => {
   try {
     const b64 = Buffer.from(req.file.buffer).toString("base64");
@@ -18,4 +20,46 @@ const handleImageUpload = async (req, res) => {
 };
 
 
-export {handleImageUpload}
+// add a new product
+const addProduct = async (req, res) => {
+  try {
+    const {
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+      averageReview,
+    } = req.body;
+
+    const newlyCreatedProduct = new Product({
+      image,
+      title,
+      description,
+      category,
+      brand,
+      price,
+      salePrice,
+      totalStock,
+      averageReview,
+    });
+
+    await newlyCreatedProduct.save();
+
+    res.status(201).json({
+      success: true,
+      data: newlyCreatedProduct,
+    });
+  } catch (error) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
+
+export { handleImageUpload, addProduct };
