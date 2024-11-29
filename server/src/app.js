dfa2a49d663d4d8cee6f659d393b4d988c43def0
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2024-11-29 22:56:26 +0600

    backend part por Cart

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,10 @@
 import express, { json } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRouter from "./routes/auth/auth.route.js"
-import adminProductsRouter from "./routes/admin/products-routes.js"
-import shopProductsRouter from './routes/shop/products-routes.js'
+import authRouter from "./routes/auth/auth.route.js";
+import adminProductsRouter from "./routes/admin/products-routes.js";
+import shopProductsRouter from "./routes/shop/products-routes.js";
+import shopCartRouter from "./routes/shop/cart-routes.js";
 
 const app = express();
 
@@ -24,10 +25,9 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/auth' , authRouter)
-app.use('/api/admin/products' , adminProductsRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/shop/products", shopProductsRouter);
-
-
+app.use("/api/shop/cart", shopCartRouter);
 
 export { app };
